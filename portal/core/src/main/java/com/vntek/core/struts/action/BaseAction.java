e641729db9ce9495754be6ba2dad16e787f7fbbc
ToiNT2 <ToiNT2@ToiNT2-PC>
2015-10-06 08:27:34 +0700

    update

--- portal/core/src/main/java/com/vntek/core/struts/action/BaseAction.java
+++ portal/core/src/main/java/com/vntek/core/struts/action/BaseAction.java
@@ -5,35 +5,36 @@ package com.vntek.core.struts.action;
 
 import java.util.Map;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.struts2.interceptor.SessionAware;
 
 import com.opensymphony.xwork2.ActionSupport;
 
 /**
  * @author ToiNT
- * @since Oct 1, 2015, 10:53:22 PM
+ * @since Oct 5, 2015, 12:59:38 PM
+ *
  */
-public class ProcessAction extends ActionSupport implements SessionAware {
+public abstract class BaseAction extends ActionSupport implements Prepareable, ModelDriven, SessionAware {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	private static Log log = LogFactory.getLog(BaseAction.class.getName());
 	private Map<String, Object> session;
 
 	/**
 	 * 
 	 *
 	 * @author ToiNT
-	 * @since Oct 5, 2015, 10:17:30 AM
+	 * @since Oct 5, 2015, 1:34:24 PM
 	 *
 	 * @param session
 	 */
 	public void setSession(Map<String, Object> session) {
 		this.session = session;
 	}
-
-	
 
 }
