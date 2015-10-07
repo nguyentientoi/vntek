fc04f7ad5af1bcdd7f5f9011868f40bb79f1f08c
nguyentientoi382 <nguyentientoi382@gmail.com>
2015-10-07 22:59:49 +0700

    commit

--- portal/core/src/main/java/com/vntek/core/struts/action/ProcessAction.java
+++ portal/core/src/main/java/com/vntek/core/struts/action/ProcessAction.java
@@ -3,10 +3,37 @@
  */
 package com.vntek.core.struts.action;
 
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+
 /**
  * @author ToiNT
  * @since Oct 1, 2015, 10:53:22 PM
  */
-public class ProcessAction {
+public class ProcessAction extends ActionSupport implements SessionAware {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private Map<String, Object> session;
+
+	/**
+	 * 
+	 *
+	 * @author ToiNT
+	 * @since Oct 5, 2015, 10:17:30 AM
+	 *
+	 * @param session
+	 */
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
+
+	
 
 }
---
+++ portal/system/src/main/java/com/vntek/system/action/AuthenticateAction.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.vntek.system.action;
+
+/**
+ * @author ToiNT
+ *
+ */
+public class AuthenticateAction {
+
+}
